b6aa20ff72bce32ffd7c129212063f3b78c09fc7
Mateo Juan A. Llanes <microbestyles@gmail.com>
2017-07-22 17:04:12 +0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -30,31 +30,32 @@ function random(limit)
     return Math.floor(Math.random() * limit);
 }
 
-
+//Returns a random word from the specified wordlist
 function word(wordlist)
 {
     return wordlist[random(wordlist.length)];
 }
 
-
+//Checks what suffix a word is using
 function suffix(s)
 {
-    return s[s.length - 1] == "s" ? "es" : "s";   
+    return s += (s[s.length - 1] == "s" ? "es" : "s");   
 }
 
 
-//Procedure
+//Function that actually generates sentences
 function GenSen()
 {
     var r;
-
-    switch(random(4 + 1))
+    
+    //I'm adding the + 1 for readability.
+    switch(random(4 + 1)) 
     {
         case 0:
             r = "Ang " + word(insultingAdjectives) + " naman.";
         break;
         case 1:
-            r = "These " + s + suffix(word(insultingNouns)) + " yeah?";
+            r = "These " + suffix(word(insultingNouns)) + " yeah?";
         break;
         case 2:
             r = "I know " + random(100) + " forms of martial arts.";
@@ -62,7 +63,7 @@ function GenSen()
         case 3:
             r = "Ang " + word(insultingAdjectives) + " naman si " + word(dogNames) + "!";
         break;
-        case 3:
+        case 4:
             r = "You all idiots"; //todo
         break;
     }
@@ -70,11 +71,10 @@ function GenSen()
     return r;
 }
 
-
+//Edits HTML file with a random sentence.
 function DrawSentence()
 {   
     var sentenceHolder = document.getElementById("sentenceHolder").textContent = GenSen();
 }
-
 
 DrawSentence();
