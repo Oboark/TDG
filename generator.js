3e420e5fa8516017e5f915e3ea3cc7841bdce11b
Mateo Juan A. Llanes <microbestyles@gmail.com>
2017-07-22 16:15:52 +0800

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,80 @@
+//Wordlists
+var insultingAdjectives = 
+[
+    "nuts",
+    "stupid",
+    "dumb"
+];
+
+
+var insultingNouns =
+[
+    "genius",
+    "idiot",
+    "moron"
+];
+
+
+var dogNames = 
+[
+    "Shobee",
+    "Oliviot",
+    "Nine",
+    "Soupie"
+]
+
+
+//Basic functions
+function random(limit)
+{
+    return Math.floor(Math.random() * limit);
+}
+
+
+function word(wordlist)
+{
+    return wordlist[random(wordlist.length)];
+}
+
+
+function suffix(s)
+{
+    return s[s.length - 1] == "s" ? "es" : "s";   
+}
+
+
+//Procedure
+function GenSen()
+{
+    var r;
+
+    switch(random(4 + 1))
+    {
+        case 0:
+            r = "Ang " + word(insultingAdjectives) + " naman.";
+        break;
+        case 1:
+            var s = word(insultingNouns);
+            r = "These " + s + suffix(s) + " yeah?";
+        break;
+        case 2:
+            r = "I know " + random(100) + " forms of martial arts.";
+        break;
+        case 3:
+            r = "Ang " + word(insultingAdjectives) + " naman si " + word(dogNames) + "!";
+        break;
+        case 3:
+            r = "You all idiots"; //todo
+        break;
+    }
+
+    return r;
+}
+
+
+function DrawSentence()
+{   
+    var sentenceHolder = document.getElementById("sentenceHolder").textContent = GenSen();
+}
+
+DrawSentence();
